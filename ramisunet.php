c8fd1ccf86c2f68c6d6a93017c41a9e97a83523e
ramikatz <roomikat@gmail.com>
2021-07-30 14:49:24 +0530

    Update ramisunet.php

--- ramisunet.php
+++ ramisunet.php
@@ -2,13 +2,13 @@
 
 /**
  * Plugin Name: Ramisunet
- * Plugin URI: https://example.com/plugins/the-basics/
+ * Plugin URI: 
  * Description: Handle the basics with this plugin.
  * Version: 1.10.3
  * Requires at least: 5.2
  * Requires PHP: 7.2
- * Author: John Smith
- * Author URI: https://author.example.com/
+ * Author: 
+ * Author URI: 
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ramisunet
